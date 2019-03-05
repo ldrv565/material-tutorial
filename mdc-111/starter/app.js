2a72a208568c41777452d2331ee17c5c1fcc0739
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2019-03-05 22:08:27 +0300

    done

--- mdc-111/starter/app.js
+++ mdc-111/starter/app.js
@@ -1,5 +1,16 @@
+import { MDCRipple } from '@material/ripple';
+import { MDCSelect } from '@material/select';
+import { MDCTextField } from '@material/textfield';
+
 const shippingForm = document.querySelector('#crane-shipping-form');
 shippingForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  alert('Success!');
+	evt.preventDefault();
+	alert('Success!');
+});
+
+new MDCRipple(document.querySelector('.mdc-button'));
+new MDCSelect(document.querySelector(".mdc-select"));
+
+[].forEach.call(document.querySelectorAll(".mdc-text-field"), function (field) {
+	new MDCTextField(field);
 });
